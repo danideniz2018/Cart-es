63f22583eeebfcf07073ba6f6799702b41f2c741
danideniz2018 <danideniz2018@gmail.com>
2024-11-04 13:35:17 -0300

    Cartões

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Como se chama um aglomerado de estrelas?',
+    'Constelação.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Do que é feito o rabo do cometa?',
+    'Gases e partículas de poeira.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Como é chamado o corpo celeste, decorrente de uma explosão de uma supernova, com um intenso campo magnético?',
+    'Magnetar.'
+)
+
+criaCartao(
+    'Astronomia',
+    'As estrelas mais quentes do universo tem a coloração...',
+    'Azul.'
+)
